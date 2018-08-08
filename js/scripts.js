430a9bed1e3e124797367b7b23117690de7c63b7
Kate Goss and Michael Harmon <kate@>
2018-08-08 12:05:39 -0700

    add function buttons for add, subtract, multiply, divide

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,39 @@ var add = function(n1, n2) {
 return n1 + n2;
 }
 
-var n1 = parseInt(prompt("Enter a number:"));
-var n2 = parseInt(prompt("Enter a second number:"));
+var subtract = function(n1, n2) {
+return n1 - n2;
+}
 
-alert(add(n1, n2));
+var multiply = function(n1, n2) {
+return n1 * n2;
+}
+
+var divide = function(n1, n2) {
+return n1 / n2;
+}
+
+$(function() {
+  $("form#numbers").submit(function(event) {
+    event.preventDefault();
+    var n1 = parseInt($("#number1").val());
+    var n2 = parseInt($("#number2").val());
+    $("#addition").click(function(){
+      var result = add(n1, n2);
+      $("#output").text(result);
+
+    });
+    $("#subtraction").click(function(){
+      var result = subtract(n1, n2);
+      $("#output").text(result);
+    });
+    $("#multiplication").click(function(){
+      var result = multiply(n1, n2);
+      $("#output").text(result);
+    });
+    $("#division").click(function(){
+      var result = divide(n1, n2);
+      $("#output").text(result);
+    });
+  });
+});
