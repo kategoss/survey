ccff612aa917d321739649d1594702576af253f5
Kate Goss and Michael Harmon <kate@>
2018-08-08 16:16:29 -0700

    add form with various form fill inserts

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,17 @@
-var add = function(n1, n2) {
-return n1 + n2;
-}
 
-var subtract = function(n1, n2) {
-return n1 - n2;
-}
-
-var multiply = function(n1, n2) {
-return n1 * n2;
-}
-
-var divide = function(n1, n2) {
-return n1 / n2;
-}
-
-$(function() {
-  $("form#numbers").submit(function(event) {
+console.log("1");
+$(document).ready(function(){
+  console.log("2");
+  $("form#numbers").submit(function(event){
+    console.log("3");
     event.preventDefault();
-    var n1 = parseInt($("#number1").val());
-    var n2 = parseInt($("#number2").val());
-    $("#addition").click(function(){
-      var result = add(n1, n2);
-      $("#output").text(result);
+    var name = $("#person1").val();
+    var dob = $("#born").val();
+    var beverage = $("#beverage").val();
 
-    });
-    $("#subtraction").click(function(){
-      var result = subtract(n1, n2);
-      $("#output").text(result);
-    });
-    $("#multiplication").click(function(){
-      var result = multiply(n1, n2);
-      $("#output").text(result);
-    });
-    $("#division").click(function(){
-      var result = divide(n1, n2);
-      $("#output").text(result);
-    });
+    var flavor = $("input:radio[name=flavor]:checked").val();
+    var favoriteColor = $("#color").val();
+
+    $("#congrats").text(name + " " + dob + beverage + flavor + favoriteColor);
   });
 });
